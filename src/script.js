8a8ae939a3da1068bfbd04b223307112dc4b7c28
Kate <ekaterina2002aleks@gmail.com>
2022-10-02 16:32:56 +0300

    Change margin

---
+++ src/script.js
@@ -0,0 +1,304 @@
+
+let pactries_1 = document.querySelector("#pactries-1");
+let pactries_1Value = document.querySelector("#pactries-1-out");
+pactries_1.addEventListener("input", () => {
+  pactries_1Value.innerHTML = pactries_1.value + " g";
+});
+
+let pactries_2 = document.querySelector("#pactries-2");
+let pactries_2Value = document.querySelector("#pactries-2-out");
+pactries_2.addEventListener("input", () => {
+  pactries_2Value.innerHTML = pactries_2.value + " g";
+});
+
+let pactries_3 = document.querySelector("#pactries-3");
+let pactries_3Value = document.querySelector("#pactries-3-out");
+pactries_3.addEventListener("input", () => {
+  pactries_3Value.innerHTML = pactries_3.value + " g";
+});
+
+let pactries_4 = document.querySelector("#pactries-4");
+let pactries_4Value = document.querySelector("#pactries-4-out");
+pactries_4.addEventListener("input", () => {
+  pactries_4Value.innerHTML = pactries_4.value + " g";
+});
+
+// berries
+
+let barberry = document.querySelector("#barberry");
+let barberryValue = document.querySelector("#barberry-out");
+barberry.addEventListener("input", () => {
+  barberryValue.innerHTML = barberry.value + " g";
+});
+let barberryKoef = 1/125;
+
+let cherry = document.querySelector("#cherry");
+let cherryValue = document.querySelector("#cherry-out");
+cherry.addEventListener("input", () => {
+  cherryValue.innerHTML = cherry.value + " g";
+});
+let cherryKoef = 1/95;
+
+let blueberries = document.querySelector("#blueberries");
+let blueberriesValue = document.querySelector("#blueberries-out");
+blueberries.addEventListener("input", () => {
+  blueberriesValue.innerHTML = blueberries.value + " g";
+});
+let blueberriesKoef = 1/140;
+
+let blackberry = document.querySelector("#blackberry");
+let blackberryValue = document.querySelector("#blackberry-out");
+blackberry.addEventListener("input", () => {
+  blackberryValue.innerHTML = blackberry.value + " g";
+});
+let blackberryKoef = 1/260;
+
+let honeysuckle = document.querySelector("#honeysuckle");
+let honeysuckleValue = document.querySelector("#honeysuckle-out");
+honeysuckle.addEventListener("input", () => {
+  honeysuckleValue.innerHTML = honeysuckle.value + " g";
+});
+let honeysuckleKoef = 1/125;
+
+let viburnum = document.querySelector("#viburnum");
+let viburnumValue = document.querySelector("#viburnum-out");
+viburnum.addEventListener("input", () => {
+  viburnumValue.innerHTML = viburnum.value + " g";
+});
+let viburnumKoef = 1/140;
+
+let dogwood = document.querySelector("#dogwood");
+let dogwoodValue = document.querySelector("#dogwood-out");
+dogwood.addEventListener("input", () => {
+  dogwoodValue.innerHTML = dogwood.value + " g";
+});
+let dogwoodKoef = 1/100;
+
+let gooseberry = document.querySelector("#gooseberry");
+let gooseberryValue = document.querySelector("#gooseberry-out");
+gooseberry.addEventListener("input", () => {
+  gooseberryValue.innerHTML = gooseberry.value + " g";
+});
+let gooseberryKoef = 1/110;
+
+let raspberry = document.querySelector("#raspberry");
+let raspberryValue = document.querySelector("#raspberry-out");
+raspberry.addEventListener("input", () => {
+  raspberryValue.innerHTML = raspberry.value + " g";
+});
+let raspberryKoef =1/125;
+
+let sea_buckthorn = document.querySelector("#sea_buckthorn");
+let sea_buckthornValue = document.querySelector("#sea_buckthorn-out");
+sea_buckthorn.addEventListener("input", () => {
+  sea_buckthornValue.innerHTML = sea_buckthorn.value + " g";
+});
+let sea_buckthornKoef = 1/170;
+
+let mountain_ash = document.querySelector("#mountain_ash");
+let mountain_ashValue = document.querySelector("#mountain_ash-out");
+mountain_ash.addEventListener("input", () => {
+  mountain_ashValue.innerHTML = mountain_ash.value + " g";
+});
+let mountain_ashKoef = 1/95;
+
+let currant = document.querySelector("#currant");
+let currantValue = document.querySelector("#currant-out");
+currant.addEventListener("input", () => {
+  currantValue.innerHTML = currant.value + " g";
+});
+let currantKoef=1/125;
+
+let turn = document.querySelector("#turn");
+let turnValue = document.querySelector("#turn-out");
+turn.addEventListener("input", () => {
+  turnValue.innerHTML = turn.value + " g";
+});
+let turnKoef =1/110;
+
+let sweet_cherry = document.querySelector("#sweet_cherry");
+let sweet_cherryValue = document.querySelector("#sweet_cherry-out");
+sweet_cherry.addEventListener("input", () => {
+  sweet_cherryValue.innerHTML = sweet_cherry.value + " g";
+});
+let sweet_cherryKoef =1/95;
+
+let mulberry = document.querySelector("#mulberry");
+let mulberryValue = document.querySelector("#mulberry-out");
+mulberry.addEventListener("input", () => {
+  mulberryValue.innerHTML = mulberry.value + " g";
+});
+let mulberryKoef =1/75;
+
+let fruit_1 = document.querySelector("#fruit-1");
+let fruit_1Value = document.querySelector("#fruit-1-out");
+fruit_1.addEventListener("input", () => {
+  fruit_1Value.innerHTML = fruit_1.value + " g";
+});
+let fruit_1_Koef =1/115;
+
+let fruit_2 = document.querySelector("#fruit-2");
+let fruit_2Value = document.querySelector("#fruit-2-out");
+fruit_2.addEventListener("input", () => {
+  fruit_2Value.innerHTML = fruit_2.value + " g";
+});
+let fruit_2_Koef =1/540;
+
+let fruit_3 = document.querySelector("#fruit-3");
+let fruit_3Value = document.querySelector("#fruit-3-out");
+fruit_3.addEventListener("input", () => {
+  fruit_3Value.innerHTML = fruit_3.value + " g";
+});
+let fruit_3_Koef =1/100;
+
+let fruit_4 = document.querySelector("#fruit-4");
+let fruit_4Value = document.querySelector("#fruit-4-out");
+fruit_4.addEventListener("input", () => {
+  fruit_4Value.innerHTML = fruit_4.value + " g";
+});
+let fruit_4_Koef =1/85;
+
+let fruit_5 = document.querySelector("#fruit-5");
+let fruit_5Value = document.querySelector("#fruit-5-out");
+fruit_5.addEventListener("input", () => {
+  fruit_5Value.innerHTML = fruit_5.value + " g";
+});
+let fruit_5_Koef =1/135;
+
+let fruit_6 = document.querySelector("#fruit-6");
+let fruit_6Value = document.querySelector("#fruit-6-out");
+fruit_6.addEventListener("input", () => {
+  fruit_6Value.innerHTML = fruit_6.value + " g";
+});
+let fruit_6_Koef =1/170;
+
+let fruit_7 = document.querySelector("#fruit-7");
+let fruit_7Value = document.querySelector("#fruit-7-out");
+fruit_7.addEventListener("input", () => {
+  fruit_7Value.innerHTML = fruit_7.value + " g";
+});
+let fruit_7_Koef =1/46;
+
+let fruit_8 = document.querySelector("#fruit-8");
+let fruit_8Value = document.querySelector("#fruit-8-out");
+fruit_8.addEventListener("input", () => {
+  fruit_8Value.innerHTML = fruit_8.value + " g";
+});
+let fruit_8_Koef =1/68;
+
+let fruit_9 = document.querySelector("#fruit-9");
+let fruit_9Value = document.querySelector("#fruit-9-out");
+fruit_9.addEventListener("input", () => {
+  fruit_9Value.innerHTML = fruit_9.value + " g";
+});
+let fruit_9_Koef =1/68;
+
+let fruit_10 = document.querySelector("#fruit-10");
+let fruit_10Value = document.querySelector("#fruit-10-out");
+fruit_10.addEventListener("input", () => {
+  fruit_10Value.innerHTML = fruit_10.value + " g";
+});
+let fruit_10_Koef =1/150;
+
+let fruit_11 = document.querySelector("#fruit-11");
+let fruit_11Value = document.querySelector("#fruit-11-out");
+fruit_11.addEventListener("input", () => {
+  fruit_11Value.innerHTML = fruit_11.value + " g";
+});
+let fruit_11_Koef =1/100;
+
+let fruit_12 = document.querySelector("#fruit-12");
+let fruit_12Value = document.querySelector("#fruit-12-out");
+fruit_12.addEventListener("input", () => {
+  fruit_12Value.innerHTML = fruit_12.value + " g";
+});
+let fruit_12_Koef =1/150;
+
+let fruit_13 = document.querySelector("#fruit-13");
+let fruit_13Value = document.querySelector("#fruit-13-out");
+fruit_13.addEventListener("input", () => {
+  fruit_13Value.innerHTML = fruit_13.value + " g";
+});
+let fruit_13_Koef =1/135;
+
+let fruit_14 = document.querySelector("#fruit-14");
+let fruit_14Value = document.querySelector("#fruit-14-out");
+fruit_14.addEventListener("input", () => {
+  fruit_14Value.innerHTML = fruit_14.value + " g";
+});
+let fruit_14_Koef =1/320;
+
+let fruit_15 = document.querySelector("#fruit-15");
+let fruit_15Value = document.querySelector("#fruit-15-out");
+fruit_15.addEventListener("input", () => {
+  fruit_15Value.innerHTML = fruit_15.value + " g";
+});
+let fruit_15_Koef =1/75;
+
+let fruit_16 = document.querySelector("#fruit-16");
+let fruit_16Value = document.querySelector("#fruit-16-out");
+fruit_16.addEventListener("input", () => {
+  fruit_16Value.innerHTML = fruit_16.value + " g";
+});
+let fruit_16_Koef =1/85;
+
+
+const push = document.querySelector("#calculate");
+const resultOut = document.querySelector("#result-calories");
+const resultArea = document.querySelector(".result-area-2");
+const outGoal1 = document.querySelector(".goal-1");
+const outGoal2 = document.querySelector(".goal-2");
+
+push.onclick = () => {
+
+  const pactries=(parseInt(pactries_1.value)+parseInt(pactries_2.value)+parseInt(pactries_3.value)+parseInt(pactries_4.value))/20;
+
+  let berries= 
+  parseInt(barberry.value)* barberryKoef +
+  parseInt(cherry.value)*cherryKoef +
+  parseInt(blueberries.value)*blueberriesKoef +
+  parseInt(blackberry.value)*blackberryKoef +
+  parseInt(honeysuckle.value)*honeysuckleKoef +
+  parseInt(viburnum.value)*viburnumKoef +
+  parseInt(dogwood.value)*dogwoodKoef +
+  parseInt(gooseberry.value)*gooseberryKoef +
+  parseInt(raspberry.value)*raspberryKoef +
+  parseInt(sea_buckthorn.value)*sea_buckthornKoef +
+  parseInt(mountain_ash.value)*mountain_ashKoef +
+  parseInt(currant.value)*currantKoef +
+  parseInt(turn.value)*turnKoef +
+  parseInt(sweet_cherry.value)*sweet_cherryKoef +
+  parseInt(mulberry.value)*mulberryKoef;
+
+  let fruits =
+  parseInt(fruit_1.value)* fruit_1_Koef +
+  parseInt(fruit_2.value)* fruit_2_Koef +
+  parseInt(fruit_3.value)* fruit_3_Koef +
+  parseInt(fruit_4.value)* fruit_4_Koef +
+  parseInt(fruit_5.value)* fruit_5_Koef +
+  parseInt(fruit_6.value)* fruit_6_Koef +
+  parseInt(fruit_7.value)* fruit_7_Koef +
+  parseInt(fruit_8.value)* fruit_8_Koef +
+  parseInt(fruit_9.value)* fruit_9_Koef +
+  parseInt(fruit_10.value)* fruit_10_Koef +
+  parseInt(fruit_11.value)* fruit_11_Koef +
+  parseInt(fruit_12.value)* fruit_12_Koef +
+  parseInt(fruit_13.value)* fruit_13_Koef +
+  parseInt(fruit_14.value)* fruit_14_Koef +
+  parseInt(fruit_15.value)* fruit_15_Koef +
+  parseInt(fruit_16.value)* fruit_16_Koef ;
+  result= berries + fruits;
+  result = Math.round(result);
+  resultOut.innerHTML = result + " BU";
+      
+    
+  if (result>40) {
+    outGoal1.innerHTML = `over`;
+    
+  } else if (result>=20) {
+    outGoal1.innerHTML = `normal`;
+    
+  } else if (result<20) {
+    outGoal1.innerHTML = `under`;
+  }
+};
